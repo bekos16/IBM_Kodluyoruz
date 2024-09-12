a541404da8d54fa65099c5a87dce2c29883b4f2e
Berke Kaya <160651249+bekos16@users.noreply.github.com>
2024-09-12 13:47:54 +0300

    Add files via upload

---
+++ euclidean.py
@@ -0,0 +1,16 @@
+from math import sqrt
+
+points = [(3, 4), (6, 4), (6, 8)]
+def euclideanDistance(point1, point2):
+    (x1, y1) = point1
+    (x2, y2) = point2
+    euclidean_distance = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return euclidean_distance
+
+distances = []
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+print(min(distances))
